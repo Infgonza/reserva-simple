8d038bd61015c4d6f8690246a42e92476b001611
Gonzalo <infantegonzalo.mdz@gmail.com>
2025-11-01 18:01:32 -0300

    primer commit

---
+++ src/main/java/com/rs/reserva_simple/ReservaSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.rs.reserva_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rs/reserva_simple/ReservaSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.reserva_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
