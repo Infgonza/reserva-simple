1cb791ec2d1c4e21bf5226bd3bfd0f9fb76eb5d2
Gonzalo <infantegonzalo.mdz@gmail.com>
2025-11-05 10:08:07 -0300

    Refactor error nombre

--- src/main/java/com/rs/reserva_simple/persistance/repository/ServicioRepository.java
+++ src/main/java/com/rs/reserva_simple/persistance/repository/ServicioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SerivicioRepository extends JpaRepository<SerivicioRepository, Long> {
+public interface ServicioRepository extends JpaRepository<ServicioRepository, Long> {
 }
