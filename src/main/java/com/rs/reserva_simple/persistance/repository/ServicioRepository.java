8e16fca42fbc2f850a2a497f1395e0bcbfd177fa
Gonzalo <infantegonzalo.mdz@gmail.com>
2025-11-05 11:27:56 -0300

    Corrigiendo error

--- src/main/java/com/rs/reserva_simple/persistance/repository/ServicioRepository.java
+++ src/main/java/com/rs/reserva_simple/persistance/repository/ServicioRepository.java
@@ -1,8 +1,9 @@
 package com.rs.reserva_simple.persistance.repository;
 
+import com.rs.reserva_simple.persistance.entity.Servicio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ServicioRepository extends JpaRepository<ServicioRepository, Long> {
+public interface ServicioRepository extends JpaRepository<Servicio, Long> {
 }
